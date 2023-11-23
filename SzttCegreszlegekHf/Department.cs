a01c5e997dfecba9011af588bf1f834ee09a7804
morzsiHUN <morzsiHUN@gmail.com>
2023-11-23 07:39:36 +0100

    initial commit

---
+++ SzttCegreszlegekHf/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzttCegreszlegekHf
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+        public int MaxEmployeeCount { get; set; }
+        public List<Department> SubDepartment { get; set; }
+    }
+}
